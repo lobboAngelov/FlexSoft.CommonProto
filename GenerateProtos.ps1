c67c1bd189414946c2799648b84c5ead8119b87b
Lyuboslav Angelov <langelov@melontech.com>
2018-03-28 13:25:46 +0300

    Added compilation for cpp and js

---
+++ GenerateProtos.ps1
@@ -0,0 +1,13 @@
+﻿$protoExetDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$ptotoRawDir = Join-Path -Path $protoExetDir -ChildPath Protos
+$dotNetProtoDir = Join-Path -Path $protoExetDir -ChildPath Protobuf.NET
+$cppProtoDir = Join-Path -Path $protoExetDir -ChildPath Protobuf.CPP
+$jsProtoDir = Join-Path -Path $protoExetDir -ChildPath Protobuf.JS
+
+.\protoc.exe $ptotoRawDir\SeverSocketMessages.proto --proto_path=$protoExetDir --csharp_out=$dotNetProtoDir --cpp_out=$cppProtoDir --js_out=$jsProtoDir
+.\protoc.exe $ptotoRawDir\ClientSocketMessages.proto --proto_path=$protoExetDir --csharp_out=$dotNetProtoDir --cpp_out=$cppProtoDir --js_out=$jsProtoDir
+
+
+Write-Host "Press any key to continue ..."
+
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
---
+++ Protobuf.JS/clienttype.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.ClientType');
+
+/**
+ * @enum {number}
+ */
+proto.ClientType = {
+  ARDUINO_CLIENT: 0,
+  WEB_CLIENT: 1
+};
+
